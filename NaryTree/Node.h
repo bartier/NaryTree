d3c0d6f680355b586f5af0c97e9c5e6ab682cccc
Vitor Bartier <vitorbartier@hotmail.com>
2018-09-03 22:45:39 -0300

    Pequenas correções

--- NaryTree/Node.h
+++ NaryTree/Node.h
@@ -49,7 +49,7 @@ public:
         if (t < keys[0])
             return 0;
 
-        for (int i = 1; i <= keys.size(); i++) {
+        for (unsigned int i = 1; i <= keys.size(); i++) {
             if (t < keys[i] && t > keys[i - 1]) {
                 return i;
             }
